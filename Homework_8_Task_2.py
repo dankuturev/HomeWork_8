006cfd6d6db2f5cb442d3f86422f7721f7a1a714
dankuturev <dan.kutyrev@yandex.ru>
2022-03-06 20:24:14 +0300

    task2 mod

--- Homework_8_Task_2.py
+++ Homework_8_Task_2.py
@@ -7,32 +7,29 @@ class YaUploader:
         self.token = token
 
     def get_headers(self):
-        return {
-            'Content-Type': 'application/json',
-            'Authorization': 'OAuth {}'.format(self.token)
-        }
+        return {'Accept': 'application/json', 'Authorization': 'OAuth {}'.format(self.token)}
 
     def _get_upload_link(self, path_to_file):
-        upload_url = "http://cloud-api.yandex.net/v1/disk/resources/upload"
+        upload_url = 'http://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = self.get_headers()
-        params = {"path": path_to_file, "owerwrite": "true"}
+        params = {'path': path_to_file, 'owerwrite': "true"}
         response = requests.get(upload_url, headers=headers, params=params)
         pprint(response.json())
         pprint(headers)
         return response.json()
 
     def upload_file(self, path_to_file, file_name):
-        href = self._get_upload_link(path_to_file=path_to_file).get("href", "")
-        response = requests.post(href, data=open(file_name, "rb"))
+        href = self._get_upload_link(path_to_file=path_to_file).get('href', '')
+        response = requests.put(href, data=open(file_name, 'rb'))
         response.raise_for_status()
-        if response.status_code == 202:
-            print("Успешно")
+        if response.status_code == 201:
+            print('Успешно')
 
 
 if __name__ == '__main__':
 
-    path_to_file =
-    file_name =
-    token =
+    path_to_file = ''
+    file_name = 'test_file.txt'
+    token = ''
     uploader = YaUploader(token)
     result = uploader.upload_file(path_to_file, file_name)
